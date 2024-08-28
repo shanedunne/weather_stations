7da6de4d802b718062d2c1fe49f8c7185ebca8f3
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2024-08-28 14:12:44 +0100

    change initial station controller to station dashboard controller. Add new station controller for handling station specific functionalities

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -2,24 +2,6 @@ import { stationStore } from "../models/station-store.js";
 
 export const stationController = {
     async index(request, response) {
-        const viewData = {
-            title : "Station Dashboard",
-            stations: await stationStore.getAllStations(),
-        }
-        console.log("station dashboard rendering");
-        response.render("dashboard-view", viewData);
-    },
-
-    async addStation(request, response) {
-        const newStation = {
-            title: request.body.title,
-        };
-        console.log(`adding new station: ${newStation.title}`);
-        await stationStore.addStation(newStation);
-        response.redirect("/dashboard");
-    },
-
-    async getStation(request, response) {
         const station = await stationStore.getStationById(request.params.id);
         const viewData = {
             title: "Station",
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
+import { stationDashboardController } from "./controllers/station-dashboard-controller.js";
 import { stationController } from "./controllers/station-controller.js";
 
 export const router = express.Router();
 
-router.get("/", stationController.index);
-router.get("/dashboard", stationController.index);
-router.post("/dashboard/addstation", stationController.addStation);
-router.get("/station/:id", stationController.getStation);
+router.get("/", stationDashboardController.index);
+router.get("/dashboard", stationDashboardController.index);
+router.post("/dashboard/addstation", stationDashboardController.addStation);
+router.get("/station/:id", stationController.index);
