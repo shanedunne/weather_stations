40ec47966ad1479876594425bc6e0641ef9a9a6c
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2024-09-16 21:12:55 +0100

    add icons and weather descriptions. add icons to temp, pressure and wind

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -9,15 +9,24 @@ export const stationController = {
     async index(request, response) {
         const station = await stationStore.getStationById(request.params.id);
         const reports = await reportStore.getAllReportsByStation(request.params.id);
-        const stationSummary = await reportStore.getStationSummary(request.params.id);  // Get weather summary
+        const stationSummary = await reportStore.getStationSummary(request.params.id);
 
-
+        // default weather code and description included here to prevent crash when new station created or incorrect weather code provided
+        let weatherInfo = {
+            icon_code: "01d",
+            weatherDescription: "No weather data available",
+        };
+    
+        if (stationSummary && stationSummary.code) {
+            weatherInfo = await weatherCodeStore.getWeatherCodeInfo(stationSummary.code);
+        }
     
         const viewData = {
             title: "Station",
             station: station,
             reports: reports,
             stationSummary: stationSummary,
+            weatherInfo: weatherInfo,
         };
         response.render("station-view", viewData);
     },
--- models/weather-code-store.js
+++ models/weather-code-store.js
@@ -8,4 +8,21 @@ export const weatherCodeStore = {
     await db.read();
     return db.data.weather_codes;
   },
+
+  async getWeatherCodeInfo(weatherCode) {
+    await db.read();
+    
+    const weatherType = await db.data.weather_codes.find((code) => code.weather_code == weatherCode);
+    const icon_code = weatherType.icon_code;
+    const weatherDescription = weatherType.weather_description;
+
+    if(!weatherType) {
+        console.log("Code does not exist");
+        icon_code = "01d";
+        weatherDescription = "Strange weather?";
+    }
+    console.log(weatherType)
+    
+    return {icon_code, weatherDescription};
+  }
 };
