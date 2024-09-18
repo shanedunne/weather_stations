61418cdf658498e2f9b65c10ea402441ba1a0147
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2024-09-18 21:15:38 +0100

    change api call from fetch to axios

--- utils/report-generator.js
+++ utils/report-generator.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { env } from "process";
 import { metrePerSecToKPH } from "./wind-speed-converter.js";
 import { whichWindDirecton } from "./wind-degrees.js";
@@ -8,26 +9,26 @@ export async function reportGenerator(stationName) {
     
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${stationName}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`;
     try {
-        const response = await fetch(api);
-        if (!response.ok) {
+        const response = await axios.get(api);
+        if (response.status !== 200) {
           throw new Error(`Response status: ${response.status}`);
         }
-    
-        const json = await response.json();
+        
+        const result = response.data;
 
         // data for report
         // round temp to nearest whole number
         // convert wind_speed from m/s to KPH and round to nearest whole number
         // convert wind direction from degrees to North etc
         const reportData = {
-            code: json.weather[0].id,
-            temp: Math.round(json.main.temp),
-            pressure: json.main.pressure,
-            wind_speed: Math.round(metrePerSecToKPH(json.wind.speed)),
-            wind_direction: whichWindDirecton(json.wind.deg),
+            code: result.weather[0].id,
+            temp: Math.round(result.main.temp),
+            pressure: result.main.pressure,
+            wind_speed: Math.round(metrePerSecToKPH(result.wind.speed)),
+            wind_direction: whichWindDirecton(result.wind.deg),
         };
 
-        // console.log(reportData);
+        console.log(reportData);
         return reportData;
       } catch (error) {
         console.error(error.message);
